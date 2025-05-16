317208bbef21d0188a9f32add363fdef52f4d369
Juan Camilo Rojas <juancamilor576@gmail.com>
2025-05-15 22:55:23 -0500

    feat: diseño de los inputs

--- src/components/StatusBarComponent.tsx
+++
@@ -1,9 +0,0 @@
-import { StatusBarProps,StatusBar } from "expo-status-bar";
-import { themeColor } from "@/utilities";
-
-type StatusProps = StatusBarProps;
-export const StatusBarComponent = ({ ...props }: StatusProps) => {
-  const isTheme = themeColor()
-
-  return <StatusBar style={isTheme} {...props} />;
-}
